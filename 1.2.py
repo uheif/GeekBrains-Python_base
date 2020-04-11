ca02aff675ec3ecaa9f499228ac0db67c106f45a
Alexander Burkat <applenoise@gmail.com>
2020-04-11 17:32:34 +0300

    удалил файлы первого урока

--- 1.2.py
+++
@@ -1,8 +0,0 @@
-seconds = int(input("Введите время в секундах: "))
-
-remain_seconds = seconds % 60
-minutes = seconds // 60
-remain_minutes = minutes % 60
-hours = minutes // 60
-
-print(f"{hours}:{remain_minutes}:{remain_seconds}")
--- 1.3.py
+++
@@ -1,5 +0,0 @@
-n = input("Введите целое положительное число: ")
-
-my_sum = int(n) + int(n * 2) + int(n * 3)
-
-#print(my_sum)
